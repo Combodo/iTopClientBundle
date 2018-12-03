05baa9377c422ab5ffa374e40521cfc618f63498
bruno DA SILVA <bruno.dasilva@combodo.com>
2018-12-03 11:07:46 +0100

    examples bugfix & minor changes

--- src/Resources/doc/examples/coreGetUserRequest.php
+++ src/Resources/doc/examples/coreGetUserRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use Combodo\ItopClientBundle\RestClient\RequestOperation\Core\RequestOperationCoreGet;
 use Combodo\ItopClientBundle\RestClient\RestClient;
@@ -10,19 +10,10 @@ $baseUrl = 'http://localhost/itop/itop-github/webservices/rest.php';
 $auth_user = 'api';
 $auth_pwd = 'api';
 
-$restClient = new RestClient($httpClient, $baseUrl, $auth_user, $auth_pwd, ['Cookie' => 'XDEBUG_SESSION=XDEBUG_ECLIPSE;']);
-
+$restClient = new RestClient($httpClient, $baseUrl, $auth_user, $auth_pwd, []);
 
 $operation = new RequestOperationCoreGet(101, 'UserRequest', 'ref, org_id');
 
 $restResponse = $restClient->executeOperation($operation);
 
-
-
-echo $restResponse->asJson();
-
-var_dump($restResponse->asArray());
-
-var_dump($restResponse->search('objects.*.fields'));
-
-var_dump($restResponse->searchOne('objects.*.fields'));
+var_dump($restResponse->search('objects.*.fields.ref'));
--- src/Resources/doc/examples/coreUpdateUserRequest.php
+++ src/Resources/doc/examples/coreUpdateUserRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use Combodo\ItopClientBundle\RestClient\RequestOperation\Core\RequestOperationCoreUpdate;
 use Combodo\ItopClientBundle\RestClient\RestClient;
